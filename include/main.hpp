15fe71e4fe2e7f96dd10b34b97cab46a7186c40f
Loo Zheng Yuan <loozhengyuan@gmail.com>
2024-09-17 16:12:53 +0800

    feat: switch board to STM32WB55RG

--- include/main.hpp
+++ include/main.hpp
@@ -1,7 +1,9 @@
 #ifndef __MAIN_H
 #define __MAIN_H
 
-#include "stm32l4xx_hal.h"
-#include "stm32l4xx_nucleo_32.h"
+#include "stm32wbxx_hal.h"
+#include "stm32wbxx_nucleo.h"
+
+void Error_Handler(void);
 
 #endif
--- include/stm32wbxx_it.h
+++ include/stm32wbxx_it.h
@@ -1,12 +1,13 @@
+/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file    Templates/Inc/stm32l4xx_it.h 
+  * @file    Templates/Inc/stm32wbxx_it.h
   * @author  MCD Application Team
   * @brief   This file contains the headers of the interrupt handlers.
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2017 STMicroelectronics.
+  * Copyright (c) 2019-2021 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -15,21 +16,37 @@
   *
   ******************************************************************************
   */
+/* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM32L4xx_IT_H
-#define __STM32L4xx_IT_H
+#ifndef __STM32WBxx_IT_H
+#define __STM32WBxx_IT_H
 
 #ifdef __cplusplus
  extern "C" {
-#endif 
+#endif
 
-/* Includes ------------------------------------------------------------------*/
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
 /* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/
-/* Exported macros -----------------------------------------------------------*/
-/* Exported functions ------------------------------------------------------- */
+/* USER CODE BEGIN ET */
 
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
 void NMI_Handler(void);
 void HardFault_Handler(void);
 void MemManage_Handler(void);
@@ -39,10 +56,12 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __STM32L4xx_IT_H */
-
+#endif /* __STM32WBxx_IT_H */
